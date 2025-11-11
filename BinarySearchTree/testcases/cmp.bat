a2665cc9e9b4feac7d8c15254457bf35e3bc59c4
pranjay977 <pranjay977@gmail.com>
2025-11-11 10:11:25 +0530

    Update cmp.bat

--- BinarySearchTree/testcases/cmp.bat
+++ BinarySearchTree/testcases/cmp.bat
@@ -1,2 +1,1 @@
-cd ..\src
-notepad bst.c
+gcc add.c -o add.exe -I ..\..\singlyLinkedList\include -I ..\..\stack\include -I ..\..\queue\include  -I ..\include -L ..\..\singlyLinkedList\lib -L ..\..\stack\lib -L ..\..\queue\lib -L ..\lib -l bstlib -l stacklib -l queuelib -l slllib
